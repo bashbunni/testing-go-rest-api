95e6d25b101398cb34d5612bbb4bb5ad81678eee
bashbunni <bashbunni.io@gmail.com>
2022-01-06 22:06:02 -0800

    feat: add endpoint GET all messages

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 )
 
+var messages []Message
+
 func welcome(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "welcome to the message board")
 	fmt.Println("Endpoint access success")
@@ -13,9 +15,15 @@ func welcome(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", welcome)
+	http.HandleFunc("/messages", GetAllMessages)
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
 func main() {
+	messages = []Message{
+		{User{1, "bob"}, "Hello!"},
+		{User{2, "tom"}, "Yo"},
+		{User{3, "joe"}, "idk"},
+	}
 	handleRequests()
 }
--- message.go
+++ message.go
@@ -1,13 +1,21 @@
 package main
 
-import "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Message struct {
-	User      User      `json: "user,omitempty"`
-	Content   string    `json: "content,omitempty"`
-	Timestamp time.Time `json: "timestamp,omitempty"`
+	User    User   `json: "user,omitempty"`
+	Content string `json: "content,omitempty"`
 }
 
-func GetAllMessages() {
+type Messages struct {
+	messages []Message
+}
 
+func GetAllMessages(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Getting all messages")
+	json.NewEncoder(w).Encode(messages)
 }
